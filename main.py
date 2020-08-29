ac3eed7cc702e78a38f404a495128fcd51209683
Minakshee25 <59467414+Minakshee25@users.noreply.github.com>
2020-08-30 01:01:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 def left():
     print("Left Hand detected - ", x1, y1, w1, h1)
+
 def right():
     print("Right hand detected - ", x1, y1, w1, h1)
+
+def both():
+    print("Both hands are detected")
 
 while(cap.isOpened()):
     ret,frame = cap.read()
@@ -26,12 +30,24 @@ while(cap.isOpened()):
         h = faces[0][3]
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)
 
-        for (x1, y1, w1, h1) in hands:
+        l=0
+        r=0
+
+        for (x1,y1,w1,h1) in hands:
             if (x1<x):
-                left()
+                cv2.rectangle(roi, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 1)
+                l=l+1
+
             elif ((x1+w1)>x):
-                right()
-            cv2.rectangle(roi, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 1)
+                cv2.rectangle(roi, (x1, y1), (x1 + w1, y1 + h1), (0, 0, 255), 1)
+                r=r+1
+
+        if l==r and (l!=0 or r!=0):
+            both()
+        elif l>r:
+            left()
+        elif l<r:
+            right()
 
     cv2.imshow('frame',frame)
 
